583f0f30da71495f9ba6fe01ea668c4f550a8e05
omairabbasi <122230181+omairabbasi@users.noreply.github.com>
2023-05-21 17:02:37 -0700

    Added support for git pages

--- src/App.js
+++ src/App.js
@@ -1,8 +1,32 @@
 import React, { useState } from 'react';
 
+window.addEventListener('load', function() {
+  const queryString = window.location.search;
+  const urlParams = new URLSearchParams(queryString);
+  const id = urlParams.get('STATE');
+  const xhr = new XMLHttpRequest();
+  xhr.open('POST', `https://albvg9jakf.execute-api.us-east-1.amazonaws.com/test/user_friends?userId=${id}`);
+  xhr.onload = function() {
+    if (xhr.status === 200) {
+      const data = JSON.parse(xhr.responseText);
+      // do something with the data
+      console.log(data);
+    } else {
+      // handle errors
+      console.error('Error:', xhr.statusText);
+    }
+  };
+  xhr.onerror = function() {
+    // handle network errors
+    console.error('Network Error');
+  };
+  xhr.send();
+});
+
 function App() {
   const [showPopup, setShowPopup] = useState(false);
-
+  
+  // some comment
   const handleButtonClick = () => {
     setShowPopup(true);
   };
